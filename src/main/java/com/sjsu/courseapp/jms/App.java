a14712712020662100d77222960de51ebce48668
Soumya Acharya <cse.soumya@gmail.com>
2015-01-10 19:05:36 -0800

    Committing request generator example
    Committing request generator example

---
+++ src/main/java/com/sjsu/courseapp/jms/App.java
@@ -0,0 +1,63 @@
+package com.sjsu.courseapp.jms;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+ 
+/**
+ * Hello world!
+ */
+public class App {
+ 
+    public static void main(String[] args) throws Exception {
+    	
+    	HelloWorldProducer producer = new HelloWorldProducer();
+    	producer.initialize();
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        Thread.sleep(1000);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        Thread.sleep(1000);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldProducer(), false);
+        Thread.sleep(1000);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+    }
+ 
+    public static void thread(Runnable runnable, boolean daemon) {
+        Thread brokerThread = new Thread(runnable);
+        brokerThread.setDaemon(daemon);
+        brokerThread.start();
+    }
+ 
+ 
+ 
+
+}
--- src/main/java/edu/sjsu/courseapp/controllers/EquipmentController.java
+++ src/main/java/edu/sjsu/courseapp/controllers/EquipmentController.java
@@ -1,31 +1,13 @@
 package edu.sjsu.courseapp.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
-//import org.npu.bankProcess.domain.Customer;
-
-import org.hibernate.mapping.Array;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.sjsu.review.httpclient.BestBuyHttpClient;
 import com.sjsu.review.httpclient.HttpClientInterface;
-
-
-import edu.sjsu.courseapp.domain.Product;
-import edu.sjsu.courseapp.domain.ProductEbay;
 import edu.sjsu.courseapp.services.ProductService;
 
 @Controller
