f97337f8535467634165428a71b3d908e79fe960
Soumya Acharya <cse.soumya@gmail.com>
2015-01-11 08:09:40 -0800

    cmmiting

--- src/main/java/edu/sjsu/courseapp/controllers/EquipmentController.java
+++ src/main/java/edu/sjsu/courseapp/controllers/EquipmentController.java
@@ -1,12 +1,19 @@
 package edu.sjsu.courseapp.controllers;
 
+import javax.jms.JMSException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.sjsu.courseapp.jms.SimpleMessageProducer;
 import com.sjsu.review.httpclient.HttpClientInterface;
 import edu.sjsu.courseapp.services.ProductService;
 
@@ -15,7 +22,6 @@ public class EquipmentController {
 
 	@Autowired
 	ProductService productservice;
-	private HttpClientInterface callBestBuyApi;
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(EquipmentController.class);
@@ -29,6 +35,16 @@ public class EquipmentController {
 	public ModelAndView newProductDataForm() {
 		ModelAndView modelView;
 		modelView = new ModelAndView("index");
+		try {
+			ApplicationContext context = 
+		             new ClassPathXmlApplicationContext("producer-jms-context.xml");
+
+		      SimpleMessageProducer messageProducer = (SimpleMessageProducer) context.getBean("messageProducer");
+		      messageProducer.sendMessages();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//modelView.addObject("product", new Product());
 		return modelView;
 	}
