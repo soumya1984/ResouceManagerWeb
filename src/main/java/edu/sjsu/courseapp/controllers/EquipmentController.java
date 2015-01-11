ce1bf99c1bad82498296376b493e1bb3cb707bf4
Soumya Acharya <cse.soumya@gmail.com>
2015-01-11 08:17:34 -0800

    commit

--- src/main/java/edu/sjsu/courseapp/controllers/EquipmentController.java
+++ src/main/java/edu/sjsu/courseapp/controllers/EquipmentController.java
@@ -1,11 +1,6 @@
 package edu.sjsu.courseapp.controllers;
 
-import javax.jms.JMSException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -14,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sjsu.courseapp.jms.SimpleMessageProducer;
-import com.sjsu.review.httpclient.HttpClientInterface;
+
 import edu.sjsu.courseapp.services.ProductService;
 
 @Controller
@@ -22,31 +17,35 @@ public class EquipmentController {
 
 	@Autowired
 	ProductService productservice;
-
-	private static final Logger logger = LoggerFactory
-			.getLogger(EquipmentController.class);
-
-	/***
-	 * product class
-	 * 
+	private static ApplicationContext context ;
+	
+	static{
+		context= new ClassPathXmlApplicationContext("producer-jms-context.xml");
+	}
+	/**
+	 * Load the index page jsp 
 	 * @return
 	 */
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView newProductDataForm() {
 		ModelAndView modelView;
 		modelView = new ModelAndView("index");
+		return modelView;
+	}
+	/**
+	 * function to call the requst genarator 
+	 * @return
+	 */
+	@RequestMapping(value = "/requestGenerator", method = RequestMethod.POST)
+	public ModelAndView newRequestGenerator() {
+		ModelAndView modelView=null;
 		try {
-			ApplicationContext context = 
-		             new ClassPathXmlApplicationContext("producer-jms-context.xml");
-
 		      SimpleMessageProducer messageProducer = (SimpleMessageProducer) context.getBean("messageProducer");
 		      messageProducer.sendMessages();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//modelView.addObject("product", new Product());
 		return modelView;
 	}
-
 }
